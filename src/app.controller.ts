e25656cce6c658bd95607909698ed46d36715f5e
Giorbis Miguel <giorbismiguel@gmail.com>
2023-01-03 20:04:48 -0500

    Create endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+
+@Controller('calculator')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post()
+  calculator(@Body('expression') expression: string): string {
+    return this.appService.evalExpression(expression);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  evalExpression(expression: string): string {
+    return eval(expression);
   }
 }
